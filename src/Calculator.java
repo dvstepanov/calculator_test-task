ce210a99195b22230cb94948fb08e5c23a3e06ba
ecko <eckowen@gmail.com>
2020-06-29 00:55:04 +0500

    2

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args){
-        System.out.println("Hi");
+        Scanner s = new Scanner(System.in);
+        System.out.println("Ввод");
+        String string = s.nextLine();
+        String[] words = string.split(" ");
+
+       /* System.out.println(words[0]);
+        System.out.println(words[1]);
+        System.out.println(words[2]);*/
+
+        switch(words[1]){
+            case "+":
+                System.out.println(Integer.parseInt(words[0]) + Integer.parseInt(words[2]));
+                break;
+            case "-":
+                System.out.println(Integer.parseInt(words[0]) - Integer.parseInt(words[2]));
+                break;
+            case "*":
+                System.out.println(Integer.parseInt(words[0]) * Integer.parseInt(words[2]));
+                break;
+            case "/":
+                System.out.println(Integer.parseInt(words[0]) / Integer.parseInt(words[2]));
+                break;
+            default:
+                System.out.println("Неверный знак операции");
+        }
+
+
     }
 }
