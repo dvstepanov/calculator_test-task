6e70dd92ffc520ab1d0beef82a15e79185738050
ecko <eckowen@gmail.com>
2020-07-17 01:59:53 +0500

    work  v 0.0011

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,39 +5,34 @@ public class Calculator {
 
         boolean flagRim = false;
         int result = 0;
+        int number1 = 0;
+        int number2 = 0;
 
-        Scanner s = new Scanner(System.in);
+        Scanner string = new Scanner(System.in);
         System.out.println("Ввод");
-        String string = s.nextLine();
-        String[] words = string.split(" ");
-
-        System.out.println(words[0]);
-        System.out.println(words[1]);
-        System.out.println(words[2]);
+        String[] words = string.nextLine().split(" ");
 
         if (words[0].matches("^[IVX]+") && words[2].matches("^[IVX]+")){
-            System.out.println("rim");
             flagRim = true;
             words[0] = String.valueOf(RomanNumeral.romanToArabic(words[0]));
             words[2] = String.valueOf(RomanNumeral.romanToArabic(words[2]));
-
         }
 
-        System.out.println(RomanNumeral.romanToArabic("MMXVIII"));
-        System.out.println(RomanNumeral.arabicToRoman(2018));
+        number1 = Integer.parseInt(words[0]);
+        number2 = Integer.parseInt(words[2]);
 
         switch(words[1]){
             case "+":
-                result = Integer.parseInt(words[0]) + Integer.parseInt(words[2]);
+                result = number1 + number2;
                 break;
             case "-":
-                result = Integer.parseInt(words[0]) - Integer.parseInt(words[2]);
+                result = number1 - number2;
                 break;
             case "*":
-                result = Integer.parseInt(words[0]) * Integer.parseInt(words[2]);
+                result = number1 * number2;
                 break;
             case "/":
-                result = Integer.parseInt(words[0]) / Integer.parseInt(words[2]);
+                result = number1 / number2;
                 break;
             default:
                 System.out.println("Неверный знак операции");
--- src/RomanNumeral.java
+++ src/RomanNumeral.java
@@ -5,8 +5,7 @@ import java.util.stream.Collectors;
 
 public enum RomanNumeral {
     I(1), IV(4), V(5), IX(9), X(10),
-    XL(40), L(50), XC(90), C(100),
-    CD(400), D(500), CM(900), M(1000);
+    XL(40), L(50), XC(90), C(100);
 
     private int value;
 
@@ -24,8 +23,7 @@ public enum RomanNumeral {
                 .collect(Collectors.toList());
     }
 
-    public static int romanToArabic(String input) {
-        String romanNumeral = input.toUpperCase();
+    public static int romanToArabic(String romanNumeral) {
         int result = 0;
 
         List<RomanNumeral> romanNumerals = RomanNumeral.getReverseSortedValues();
@@ -43,16 +41,13 @@ public enum RomanNumeral {
         }
 
         if (romanNumeral.length() > 0) {
-            throw new IllegalArgumentException(input + " cannot be converted to a Roman Numeral");
+            throw new IllegalArgumentException(romanNumeral + " cannot be converted to a Roman Numeral");
         }
 
         return result;
     }
 
     public static String arabicToRoman(int number) {
-        if ((number <= 0) || (number > 4000)) {
-            throw new IllegalArgumentException(number + " is not in range (0,4000]");
-        }
 
         List<RomanNumeral> romanNumerals = RomanNumeral.getReverseSortedValues();
 
