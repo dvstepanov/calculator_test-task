bc3ed19b1d38e6a8278c525ad52d7cbd936c6bdc
ecko <eckowen@gmail.com>
2020-07-17 01:28:37 +0500

    work  v 0.001

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[] args){
 
-      /*  boolean flagRim = false;
+        boolean flagRim = false;
+        int result = 0;
 
         Scanner s = new Scanner(System.in);
         System.out.println("Ввод");
@@ -16,27 +17,38 @@ public class Calculator {
 
         if (words[0].matches("^[IVX]+") && words[2].matches("^[IVX]+")){
             System.out.println("rim");
-        }
-*/
-        System.out.println(RomanNumerals.toInt("VII"));
+            flagRim = true;
+            words[0] = String.valueOf(RomanNumeral.romanToArabic(words[0]));
+            words[2] = String.valueOf(RomanNumeral.romanToArabic(words[2]));
 
-      /*  switch(words[1]){
+        }
+
+        System.out.println(RomanNumeral.romanToArabic("MMXVIII"));
+        System.out.println(RomanNumeral.arabicToRoman(2018));
+
+        switch(words[1]){
             case "+":
-                System.out.println(Integer.parseInt(words[0]) + Integer.parseInt(words[2]));
+                result = Integer.parseInt(words[0]) + Integer.parseInt(words[2]);
                 break;
             case "-":
-                System.out.println(Integer.parseInt(words[0]) - Integer.parseInt(words[2]));
+                result = Integer.parseInt(words[0]) - Integer.parseInt(words[2]);
                 break;
             case "*":
-                System.out.println(Integer.parseInt(words[0]) * Integer.parseInt(words[2]));
+                result = Integer.parseInt(words[0]) * Integer.parseInt(words[2]);
                 break;
             case "/":
-                System.out.println(Integer.parseInt(words[0]) / Integer.parseInt(words[2]));
+                result = Integer.parseInt(words[0]) / Integer.parseInt(words[2]);
                 break;
             default:
                 System.out.println("Неверный знак операции");
         }
 
-*/
+        if (flagRim) {
+            System.out.println(RomanNumeral.arabicToRoman(result));
+        }else {
+            System.out.println(result);
+        }
+
+
     }
 }
