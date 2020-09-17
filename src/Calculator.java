550a7ce9193fe08c0148c3ec9b98e10635f24b75
dvstepanov <eckowen@gmail.com>
2020-09-17 14:38:11 +0500

    work v1

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,26 +1,34 @@
 import java.util.Scanner;
 
 public class Calculator {
+    private static boolean flagRim = false;
+    private static int result = 0;
+    private static int number1 = 0;
+    private static int number2 = 0;
+    private static String operation;
+
     public static void main(String[] args){
+        read();
+        calculate(number1, number2, operation);
+        printResult();
+    }
 
-        boolean flagRim = false;
-        int result = 0;
-        int number1 = 0;
-        int number2 = 0;
-
+    private static void read() {
         Scanner string = new Scanner(System.in);
-        System.out.println("Введите два чилса строго от 1 до 10 включительно и знак операции между ними. " +
-                            "Операция деления происходит с округление до целого числа!");
+        System.out.println("Введите два числа строго от 1 до 10 включительно и знак операции между ними. " +
+                "Операция деления происходит с округление до целого числа!");
         String[] words = string.nextLine().split(" ");
 
-        if (words[0].matches("^[IVX]+") && words[2].matches("^[IVX]+")){
-            flagRim = true;
+       validation(words[0], words[2]);
+
+        if (flagRim){
             words[0] = String.valueOf(RomanNumeral.romanToArabic(words[0]));
             words[2] = String.valueOf(RomanNumeral.romanToArabic(words[2]));
         }
 
         number1 = Integer.parseInt(words[0]);
         number2 = Integer.parseInt(words[2]);
+        operation = words[1];
 
         try{
             if(!((number1 > 0) && (number1 <= 10) && (number2 > 0) && (number2 <= 10))){
@@ -28,12 +36,28 @@ public class Calculator {
             }
         }
         catch(Exception ex){
-
             System.err.println(ex.getMessage());
-            main(args);
+            System.exit(0);
         }
+    }
 
-        switch(words[1]){
+    private static void validation(String num1, String num2) {
+        if(num1.matches("^[IVX]+") & num2.matches("^[IVX]+")){
+            flagRim = true;
+        }
+        try{
+            if(num1.matches("^[IVX]+") ^ num2.matches("^[IVX]+")){
+                throw new Exception("Числа должны быть одной системы исчисления!(Арабской или Римской)");
+            }
+        }
+        catch(Exception ex){
+            System.err.println(ex.getMessage());
+            System.exit(0);
+        }
+    }
+
+    public static void calculate(int number1, int number2, String operation) {
+        switch(operation){
             case "+":
                 result = number1 + number2;
                 break;
@@ -47,9 +71,10 @@ public class Calculator {
                 try {
                     result = number1 / number2;
                     break;
-                } catch (ArithmeticException ex){
+                }
+                catch (ArithmeticException ex){
                     System.err.println("Деление на ноль!");
-                    return;
+                    System.exit(0);
                 }
             default:
                 try{
@@ -57,14 +82,17 @@ public class Calculator {
                 }
                 catch(Exception ex){
                     System.err.println(ex.getMessage());
-                    return;
+                    System.exit(0);
                 }
         }
+    }
 
+    private static void printResult() {
         if (flagRim) {
             System.out.println(RomanNumeral.arabicToRoman(result));
-        }else {
+        } else {
             System.out.println(result);
         }
     }
+
 }
